71b6cd98ef02a4a73120ad331f277df92bba385a
Kevin Yang <kyang@altigen.com>
2019-10-14 15:29:46 +0800

    initial

---
+++ 001.SignIn.CP/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _001.SignIn.CP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ 002.SignIn.RS/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _002.SignIn.RS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
